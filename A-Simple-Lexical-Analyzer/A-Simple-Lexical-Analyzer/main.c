6f7d316fb5001b677b9fdffe290065241920cb6e
王啸川 <m15048619182@163.com>
2018-09-30 17:14:31 +0800

    << init a new project

---
+++ A-Simple-Lexical-Analyzer/A-Simple-Lexical-Analyzer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  A-Simple-Lexical-Analyzer
+//
+//  Created by 王啸川 on 2018/9/30.
+//  Copyright © 2018年 王啸川. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
