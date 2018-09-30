8ef2e22b618b451d0fa5fd3eb0132f422ac3c1cc
bitzhangmo <m15048619182@163.com>
2018-10-01 03:22:38 +0800

    << windows版

---
+++ A-Simple-Lexical-Analyze-windows/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  A-Simple-Lexical-Analyzer
+//
+//  Created by 王啸川 on 2018/9/30.
+//  Copyright © 2018年 王啸川. All rights reserved.
+//
+
+#include "lexicalAnalyzer.h"
+int main(int argc, const char * argv[]) {
+    sourceReader();
+    Debug();
+    return 0;
+}
